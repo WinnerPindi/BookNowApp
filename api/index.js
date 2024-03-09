efab9894cd033a87a202d3949bacf969dcdaae69
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-03-09 12:55:01 -0500

    First commit

---
+++ api/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/authRoute.js";
+import usersRoute from "./routes/usersRoute.js";
+import roomsRoute from "./routes/roomsRoute.js";
+import hotelsRoute from "./routes/hotelsRoute.js";
+const app = express();
+dotenv.config();
+
+//Fonction qui permet de se connecter à MongoDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    console.error("Falied to connect to MongoDB", error);
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
+
+//Middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+
+app.listen(8800, () => {
+    //Appel de la fonction de connexion à Moogo
+  connect();
+  console.log("Connected to backend");
+});

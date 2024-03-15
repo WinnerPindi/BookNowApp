8799e2f4e7acd02e08a3c57ea2cdc1a8124c5416
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-03-15 14:56:10 -0400

    Ajout de la connexion dans l'API

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,17 +1,35 @@
 import UserModel from "../models/UserModel.js"
-
+import bcrypt from 'bcryptjs'
 export const register = async(req,res,next) =>{
     try{
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new UserModel({
             lastname:req.body.lastname,
             firstname:req.body.firstname,
             email:req.body.email,
-            password:req.body.password,
+            password:hash,
         });
 
         await newUser.save();
         res.status(200).send("User has been created");
-    }catch{
-        
+    }catch(err){
+        next(err);
+    }
+}
+
+export const login = async(req,res,next) =>{
+    try{
+     const user = await UserModel.findOne({email:req.body.email})
+     if(!user) return next(createError(404, "User not found"));
+
+     const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+     if(!isPasswordCorrect) return next(createError(404, "Wrong password or username"));
+
+     const {password, isAdmin, ...otherDetails } = user._doc;
+     res.status(200).json({...otherDetails});
+    }catch(err){
+        next(err);
     }
 }
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
-
+router.post("/login", login);
 export default router;

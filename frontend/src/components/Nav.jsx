cf446d38713b65e42b8454894970124b8d420c98
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-03-16 11:57:09 -0400

    Fixation de la barre de navigation

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,19 +2,11 @@ import { Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import BookingLogo from "../assets/lit.png";
 import { useState } from "react";
-import {useSelector} from "react-redux"
-// Mise à jour pour inclure les routes sous forme d'objets contenant le nom et le chemin
-const ROUTES = [
-  { name: "Accueil", path: "/" },
-  { name: "Logement", path: "/logement" }, // Vous devez ajouter cette route dans vos <Routes> si ce n'est pas déjà fait
-  { name: "Mes Reservations", path: "/reservations" }, // Ajoutez cette route également
-  //{ name: "Connexion", path: "/login" },
-];
+import { useSelector } from "react-redux";
 
 export function Nav() {
   const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
-  const {user} = useSelector((state) => state.authSlice)
-  console.log(user);
+  const { user } = useSelector((state) => state.authSlice);
 
   return (
     <nav className="flex flex-wrap justify-between items-center">
@@ -30,15 +22,20 @@ export function Nav() {
       {/* Menu */}
       <div className={`${!isMobileMenuShown && "hidden"} w-full lg:w-auto lg:block`}>
         <ul className="lg:space-x-8 flex flex-col lg:flex-row bg-gray-50 lg:bg-transparent text-lg border border-gray-100 lg:border-none rounded-lg p-4">
-          {ROUTES.map((route) => (
-            <li key={route.name} className={`px-3 py-2 cursor-pointer hover:bg-gray-100 ${route.path === "/" ? "bg-amber-700 text-white lg:bg-transparent lg:text-amber-700" : ""}`}>
-              <Link to={route.path} className="block">
-                {route.name}
-              </Link>
-              
-            </li>
-          ))}
-          {user?.userDetails?<h1>{user.userDetails.lastname}</h1>:<Link to="/login">Connexion</Link>}
+          {/* Ici, les routes sont séparées individuellement en utilisant des balises <li> et <Link> */}
+          <li className="px-3 py-2 cursor-pointer hover:bg-gray-100 bg-amber-700 text-white lg:bg-transparent lg:text-amber-700">
+            <Link to="/" className="block">Accueil</Link>
+          </li>
+          <li className="px-3 py-2 cursor-pointer hover:bg-gray-100">
+            <Link to="/logement" className="block">Logement</Link>
+          </li>
+          <li className="px-3 py-2 cursor-pointer hover:bg-gray-100">
+            <Link to="/reservations" className="block">Mes Reservations</Link>
+          </li>
+          {/* Condition pour afficher le nom de l'utilisateur ou le lien de connexion */}
+          {user?.userDetails
+            ? <li className="px-3 py-2 cursor-pointer hover:bg-gray-100">{user.userDetails.lastname}</li>
+            : <li className="px-3 py-2 cursor-pointer hover:bg-gray-100"><Link to="/login" className="block">Connexion</Link></li>}
         </ul>
       </div>
     </nav>
